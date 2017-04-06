769fee9318ffd7af03538621e7fcf85eef4a7ddc
Yulia Poganyuko <yulia.poganyuko@gmail.com>
2017-04-07 02:57:28 +0300

    Add IFileStorage

---
+++ Task2Logic/BinaryFilesStorage.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2Logic
+{
+    public class BinaryBooksStorage: IFileStorage<Book>
+    {
+        private readonly string filePath;
+
+        public BinaryBooksStorage()
+        {
+            string folderPath = AppDomain.CurrentDomain.BaseDirectory;
+            filePath = Path.Combine(folderPath, "books");
+        }
+
+        public BinaryBooksStorage(string path)
+        {
+            if (String.IsNullOrEmpty(filePath))
+                throw new ArgumentNullException();
+            filePath = path;
+        }
+
+        public IEnumerable<Book> Load()
+        {
+            List<Book> books = new List<Book>();
+
+            if (!File.Exists(filePath))
+                throw new FileNotFoundException();
+            Stream stream = File.Open(filePath, FileMode.Open, FileAccess.Read);
+            using (BinaryReader reader = new BinaryReader(stream))
+            {
+                while (reader.BaseStream.Position < reader.BaseStream.Length)
+                {
+                    Book book = new Book();
+                    book.Author = reader.ReadString();
+                    book.Title = reader.ReadString();
+                    book.PublishingHouse = reader.ReadString();
+                    book.Year = reader.ReadInt32();
+                    book.Genre = reader.ReadString();
+                    books.Add(book);
+                }
+            }
+            return books;
+        }
+
+        public void Save(IEnumerable<Book> books)
+        {
+            if (ReferenceEquals(books, null))
+                throw new ArgumentNullException();
+            Stream stream = new FileStream(filePath, FileMode.Create, FileAccess.Write);
+            using (BinaryWriter writer = new BinaryWriter(stream))
+            {
+                foreach (Book book in books)
+                {
+                    writer.Write(book.Author);
+                    writer.Write(book.Title);
+                    writer.Write(book.PublishingHouse);
+                    writer.Write(book.Year);
+                    writer.Write(book.Genre);
+                }
+            }
+        }
+    }
+}
---
+++ Task2Logic/IFileStorage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task2Logic
+{
+    /// <summary>
+    /// Provides functionality for work with a file storage.
+    /// </summary>
+    /// <typeparam name="T"> Type of files in the storage</typeparam>
+    interface IFileStorage<T>
+    {
+        /// <summary>
+        /// Loads files from storage.
+        /// </summary>
+        /// <returns> IEnumerable containing files</returns>
+        IEnumerable<T> Load();
+
+        /// <summary>
+        /// Saves IEnumerable containing files to storage.
+        /// </summary>
+        /// <param name="files"> IEnumerable containing files</param>
+        void Save(IEnumerable<T> files);
+    }
+}
