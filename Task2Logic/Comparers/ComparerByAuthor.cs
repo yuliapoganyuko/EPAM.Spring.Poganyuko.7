3b3b9c73c2163a49f7d9cd432a48b7a2bd622f9b
Yulia Poganyuko <yulia.poganyuko@gmail.com>
2017-04-07 08:43:03 +0300

    Add Comparers

---
+++ Task2Logic/Comparers/ComparerByAuthor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Task2Logic.Comparers
+{
+    public class ComparerByAuthor: IComparer<Book>
+    {
+        public int Compare(Book firstBook, Book secondBook)
+        {
+            if (!ReferenceEquals(firstBook, null))
+                return firstBook.Author.CompareTo(secondBook.Author);
+            if (ReferenceEquals(secondBook, null))
+                return 0;
+            return -1;
+        }
+    }
+}
---
+++ Task2Logic/Comparers/ComparerByTitle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task2Logic.Comparers
+{
+    public class ComparerByTitle: IComparer<Book>
+    {
+        public int Compare(Book firstBook, Book secondBook)
+        {
+            if (!ReferenceEquals(firstBook, null))
+                return firstBook.Title.CompareTo(secondBook.Title);
+            if (ReferenceEquals(secondBook, null))
+                return 0;
+            return -1;
+        }
+    }
+}
