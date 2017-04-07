746c1e3c209426ceef11fe388ba3ec386ffeaa04
Yulia Poganyuko <yulia.poganyuko@gmail.com>
2017-04-07 08:54:53 +0300

    Add test of ComparerByAuthor

--- Task2Console/Program.cs
+++ Task2Console/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using Task2Logic;
+using Task2Logic.Comparers;
 
 namespace Task2Console
 {
@@ -16,21 +17,29 @@ namespace Task2Console
             Book bookToRemove = new Book("First author", "First book", "First publishing house", 2001, "First genre");
 
             service.AddBook(bookToRemove);
-            service.AddBook(new Book("Second author", "Second book", "Second publishing house", 2002, "Second genre"));
+            service.AddBook(new Book("Undefined author", "Second book", "Second publishing house", 2002, "Second genre"));
             service.AddBook(new Book("Third author", "Third book", "Third publishing house", 2003, "Third genre"));
 
             Console.WriteLine("Books in the storage:");
             List<Book> books = storage.Load().ToList();
             foreach (var book in books)
                 Console.WriteLine(book.ToString());
+            Console.WriteLine();
 
-            Console.WriteLine("Book founded by tag Author == \"Second author\":");
-            Book foundedBook = service.FindByTag(x => x.Author == "Second author");
+            Console.WriteLine("Book founded by tag Author == \"First author\":");
+            Book foundedBook = service.FindByTag(x => x.Author == "First author");
             Console.WriteLine(foundedBook.ToString());
 
             Console.WriteLine("Book founded by tag Title == \"Third book\":");
             foundedBook = service.FindByTag(x => x.Title == "Third book");
-            Console.WriteLine(foundedBook.ToString());
+            Console.WriteLine(foundedBook.ToString() + "\n");
+
+            service.SortBooksByTag(new ComparerByAuthor());
+            Console.WriteLine("Sorted by author books in the storage:");
+            books = storage.Load().ToList();
+            foreach (var book in books)
+                Console.WriteLine(book.ToString());
+            Console.WriteLine();
 
             service.RemoveBook(bookToRemove);
             Console.WriteLine("Books in the storage after removing first book:");
